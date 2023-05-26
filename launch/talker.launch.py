7de23bb362c73a67404a0b059fc8d420bf20bcfe
Younes-boutelidjane <boutelidjaneyounes@gmail.com>
2023-05-26 18:10:38 +0200

    my first commit

---
+++ launch/talker.launch.py
@@ -0,0 +1,10 @@
+from launch import LaunchDescription
+from launch.action import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package = 'demo_nodes_py',
+            executable= 'talker'
+        )
+    ])
